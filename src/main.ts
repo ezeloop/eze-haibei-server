3c896c9afe59bcf46640db9f80f0a019d4895e86
Marcos Lopez <ezequiel.lopez@externos-cl.cencosud.com>
2024-06-17 17:33:16 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
+  /* app.enableCors({
     origin: ['http://localhost:3000', process.env.FRONT_END_URL],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }); */
+  app.enableCors({
+    origin: true,
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization',
+    methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
   
   await app.listen(4000);
-    return app.getHttpAdapter().getInstance();
+    //return app.getHttpAdapter().getInstance();
 
 }
 bootstrap();
