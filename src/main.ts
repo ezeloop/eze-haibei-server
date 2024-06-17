2e1f590c0ec5e80c08bf60b7d4f15f17bb3d9b1c
Marcos Lopez <ezequiel.lopez@externos-cl.cencosud.com>
2024-06-17 11:40:49 -0300

    fix:

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Your frontend URL
+    origin: process.env.FRONT_END_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
